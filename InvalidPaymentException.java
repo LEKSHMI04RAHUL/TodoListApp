3c21ac5da2a9c9f6095e587f8bb6266ab1e218cd
LEKSHMI04RAHUL <rahullekshmi1@gmail.com>
2024-09-08 11:14:06 +0100

    Delete InvalidPaymentException.java

--- InvalidPaymentException.java
+++
@@ -1,8 +0,0 @@
-package com.ust.ecomerce;
-
-//InvalidPaymentException.java
-public class InvalidPaymentException extends Exception {
-public InvalidPaymentException(String message) {
-   super(message);
-}
-}
